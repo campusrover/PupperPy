23b33f293d4dbad70a5cd97081f5a565d1111ca0
Roshan <rnanu@brandeis.edu>
2020-11-08 17:03:45 -0500

    fix infinite loop issue

--- pupperpy/imu_tools.py
+++ pupperpy/imu_tools.py
@@ -73,15 +73,16 @@ class IMU(object):
 
     def initSensor(self):
         try_count = 0
-        try:
-            self.i2c = I2C(SCL_GPIO, SDA_GPIO)
-            self.sensor = BNO055_I2C(self.i2c)
-        except:
-            if try_count > 3:
-                raise
-            else:
-                try_count += 1
-                self.initSensor()
+        connected = False
+        while not connected:
+            try:
+                self.i2c = I2C(SCL_GPIO, SDA_GPIO)
+                self.sensor = BNO055_I2C(self.i2c)
+            except:
+                if try_count > 3:
+                    raise
+                else:
+                    try_count += 1
 
     def read(self):
         calibration_status = self.sensor.calibration_status
