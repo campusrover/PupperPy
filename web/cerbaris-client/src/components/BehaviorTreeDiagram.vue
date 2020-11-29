ef5f6f182cc30e129d295a1be2f168049fcb9ccd
Evalyn Berleant <eberleant@gmail.com>
2020-11-29 01:01:38 -0500

    Improve tree diagram

--- web/cerbaris-client/src/components/BehaviorTreeDiagram.vue
+++ web/cerbaris-client/src/components/BehaviorTreeDiagram.vue
@@ -4,10 +4,10 @@
 
 <script>
   import * as joint from 'jointjs'
+  import dagre from 'dagre'
+  import graphlib from 'graphlib'
 
   // todo: get string from GET request to API
-  // move analyzing node type into tokenizer
-  // separate different graphs
   // switch between normal dashboard and this using keyboard shortcut
   // make fully interactive
   const behaviorTreeString = 
@@ -20,7 +20,6 @@
 \t\t\tcontinue
 \t\t\t\tmove towards ball
 \t\twait
-
 look for ball
 \tthen
 \t\tturn 432 degrees
@@ -47,6 +46,8 @@ move towards ball
 \t\t\tcontinue
 \t\t\t\tmove forward
 `
+/*
+*/
 
   const BehaviorTreeDiagram = {
     components: {},
@@ -56,48 +57,55 @@ move towards ball
       }
     },
     mounted() {
-      this.drawBehaviorTree(this.parseTree(this.tokenize(behaviorTreeString), 0))
+      this.context = document.createElement('canvas').getContext('2d')
+      this.font = {
+        size: 14,
+        family: 'Helvetica',
+      }
+      this.context.font = this.font.size + 'px ' + this.font.family
+      this.drawTree(this.parseTree(this.tokenize(behaviorTreeString), 0))
     },
     methods: {
       tokenize(text) {
         let lines = text.split('\n')
         let tokens = []
         tokens.push({tabs: -1, value: null}) // root
+
         lines.forEach(line => {
           if (line.trim().length === 0) {
             return
           }
           let tabs = line.lastIndexOf('\t') + 1
-          let value = line.slice(tabs)
-          tokens.push({tabs, value})
+          let value = line.trim()
+          let type = this.determineType(tabs, value)
+          tokens.push({tabs, value, type})
         })
         return tokens
       },
 
-      analyzeLevels(tokens) {
-        let levels = []
-        tokens.forEach(token => {
-          if (token.tabs < 0) {
-            return
-          }
-          if (levels[token.tabs] === undefined) {
-            levels[token.tabs] = 1
-          } else {
-            levels[token.tabs]++
-          }
-        })
-        return levels
+      determineType(tabs, value) {
+        if (value.startsWith('then')) {
+          return 'then'
+        } else if (value.startsWith('unless')) {
+          return 'condition'
+        } else if (tabs === 0) {
+          return 'definition'
+        } else {
+          return 'action'
+        }
       },
 
       parseTree(tokens, index) {
+        let node = {
+          value: tokens[index].value,
+          type: tokens[index].type,
+        }
         // base case: leaf node; no next elt or next elt's tabs <= curr elt's tabs
         if (tokens.length <= index + 1 || tokens[index + 1].tabs <= tokens[index].tabs) {
-          return {
-            value: tokens[index].value,
-            children: [],
-          }
+          node.children = []
+          return node
         } else {
-          // loop through children
+          // loop through children and call recursively
           let childIndex = index + 1
           let children = []
           while (childIndex < tokens.length && tokens[childIndex].tabs > tokens[index].tabs) {
@@ -106,14 +114,12 @@ move towards ball
             }
             childIndex++
           }
-          return {
-            value: tokens[index].value,
-            children,
-          }
+          node.children = children
+          return node
         }
       },
 
-      drawBehaviorTree(treeObj) {
+      drawTree(treeObj) {
         let graph = new joint.dia.Graph
         let paperSize = {width: window.innerWidth, height: window.innerHeight}
 
@@ -126,60 +132,78 @@ move towards ball
         })
 
         let tokens = this.tokenize(behaviorTreeString)
-        let levels = this.analyzeLevels(tokens)
-        let margin = 40
-        let node = new joint.shapes.standard.Rectangle()
-        node.resize(0, paperSize.height/levels.length - margin*2)
-        node.attr({
-
+        this.drawNode(graph, null, this.parseTree(tokens, 0))
+        let graphBBox = joint.layout.DirectedGraph.layout(graph, {
+          dagre: dagre,
+          graphlib: graphlib,
+          nodeSep: 40,
+          edgeSep: 80,
+          marginX: 20,
+          marginY: 20,
+          rankDir: "LR",
         })
 
-        this.drawNode(paperSize, margin, levels, new Array(levels.length).fill(0), graph, null, this.parseTree(tokens, 0), -1)
+        paper.setDimensions(window.innerWidth, graphBBox.height + 40)
       },
 
-      drawNode(paperSize, margin, levelCaps, levelFilled, graph, parent, nodeObj, level) {
-        console.log(parent)
+      drawNode(graph, parent, nodeObj) {
         let node = new joint.shapes.standard.Rectangle()
-        let width = paperSize.width/levelCaps[level] - margin*2
-        let height = paperSize.height/levelCaps.length - margin*2
-        node.resize(width, height)
+        // calculate dimensions
+        let width = 120
         let wrapText = joint.util.breakText(nodeObj.value || '', {width})
-        node.attr({
-          label: {
-            text: wrapText,
-            fill: 'black',
-          }
-        })
-        this.styleNode(node)
-        node.position((width + margin*2) * levelFilled[level] + margin, 
-          (height + margin*2) * level + margin)
+        let {height} = this.getTextSize(wrapText)
+        node.resize(width + 25, height + 25)
+        // style node and add label
+        this.styleNode(node, wrapText, nodeObj.type)
         // draw self
-        if (level >= 0) {
+        if (nodeObj.value !== null) {
           node.addTo(graph)
         }
         // draw link
-        if (level > 0) {
-          let link = new joint.shapes.standard.Link();
-          console.log(parent.attr('label/text'))
+        if (parent && parent.attr('label/text').length > 0) {
+          let link = new joint.shapes.standard.Link()
+          link.attr('line/strokeWidth', 1)
           link.source(parent)
           link.target(node)
           link.addTo(graph)
         }
         // draw children
-        levelFilled[level]++
-        console.log(node)
         nodeObj.children.forEach(child => {
-          this.drawNode(paperSize, margin, levelCaps, levelFilled, graph, node, child, level + 1)
+          this.drawNode(graph, node, child)
         })
       },
 
-      styleNode(node) {
-        let value = node.attr('label/text')
-        if (value.startsWith('then')) {
+      getTextSize(text, font) {
+        let lines = text.split('\n')
+        let maxWidth = 0
+        lines.forEach(line => {
+          let currWidth = this.context.measureText(line).width
+          if (currWidth > maxWidth) {
+            maxWidth = currWidth
+          }
+        })
+        let metrics = this.context.measureText(text);
+        let height = 1.4 * lines.length * (metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent)
+        return {width: maxWidth, height};
+      },
+
+      styleNode(node, text, type) {
+        node.attr({
+          body: {
+            strokeWidth: 1,
+          },
+          label: {
+            text: text,
+            fill: 'black',
+            fontFamily: this.font.family,
+            fontSize: this.font.size,
+          },
+        })
+        if (type === 'then') {
           node.attr('body/fill', 'lightcyan')
-        } else if (value.startsWith('unless')) {
+        } else if (type === 'condition') {
           node.attr('body/fill', 'lightyellow')
-        } else if (value.startsWith('wait') || value.startsWith('continue')) {
+        } else if (type === 'action') {
           node.attr('body/fill', 'moccasin')
         } else {
           node.attr('body/fill', 'pink')
