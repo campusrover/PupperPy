dee8ed5f186300e5cb64aebcbd95972822003c98
Roshan Nanu <roshan.nanu@gmail.com>
2020-12-11 18:33:31 -0500

    addded pigpiod start-up and safe termination

--- pupperpy/run_cerbaris.py
+++ pupperpy/run_cerbaris.py
@@ -7,9 +7,16 @@ PATH = os.path.dirname(os.path.abspath(__file__))
 VISION_SCRIPT = os.path.join(PATH, 'Vision', 'pupper_vision.py')
 
 if __name__ == "__main__":
-    control = Control()
-    vis_p = Popen(['python3', VISION_SCRIPT])
-    robo_p = Popen(['python3', RUN_ROBOT_PATH])
-    time.sleep(5)
-    control.run_loop()
+    try:
+        pigpio_p = Popen(['pigpiod'])
+        control = Control()
+        vis_p = Popen(['python3', VISION_SCRIPT])
+        robo_p = Popen(['python3', RUN_ROBOT_PATH])
+        time.sleep(5)
+        control.run_loop()
+    except:
+        pigpio_p.terminate()
+        control.stop_loop()
+        vis_p.kill()
+        robo_p.kill()
 
