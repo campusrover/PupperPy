a1957facabedeba73fe691de1c611ccce61a1077
Christopher Tam <christophertam@brandeis.edu>
2020-12-11 14:53:16 -0500

    fix attribute name

--- pupperpy/Behavior/pupper_actions.py
+++ pupperpy/Behavior/pupper_actions.py
@@ -16,7 +16,7 @@ class MoveUntilObstaclesNode(py_trees.behaviour.Behaviour):
         return
 
     def update(self):
-        obj = self.tsh.control.obj
+        obj = self.tsh.control.obj_sensors
         if any(obj.values()):
             self.feedback_message = "End of journey!"
             return py_trees.common.Status.SUCCESS
@@ -47,7 +47,7 @@ class AvoidObstaclesNode(py_trees.behaviour.Behaviour):
         return
 
     def update(self):
-        obj = self.tsh.control.obj
+        obj = self.tsh.control.obj_sensors
         if any(obj.values()):
             self.feedback_message = "Dodging obstacle..."
             self.tsh.active_node = self
